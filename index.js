b5dcb81649acaa9150b372307ddef80efbb60e60
Ignacio Cunial <igsecu@hotmail.com>
2023-08-20 20:47:32 -0300

    Res Headers middleware -> index.js

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Body-Parse middleware
+// Body-Parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Res Headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
