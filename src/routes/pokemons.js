b3d9b6726e31ff15949f8d67ec14be4ba1f1143e
Ignacio Cunial <igsecu@hotmail.com>
2023-08-21 20:34:29 -0300

    Created GET /:id route -> get pokemon by its id

--- src/routes/pokemons.js
+++ src/routes/pokemons.js
@@ -3,6 +3,29 @@ const router = express.Router();
 
 const pokemonsController = require("../controllers/pokemons");
 
+// Get pokemon by its id
+router.get("/:id", async (req, res, next) => {
+  const id = req.params.id;
+  let result = [];
+
+  try {
+    result = await pokemonsController.findPokemonByIdApi(id);
+
+    if (!result.length)
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `Pokemon with ID: ${id} not found!`,
+      });
+
+    res.status(200).json({
+      statusCode: 200,
+      data: result,
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
 // Get all pokemons
 router.get("/", async (req, res, next) => {
   try {
