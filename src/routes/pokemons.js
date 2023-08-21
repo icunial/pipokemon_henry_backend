7a2fb0bace39302b6fc3721b412b6a68ec85a98f
Ignacio Cunial <igsecu@hotmail.com>
2023-08-21 15:53:49 -0300

    Created GET / route -> get all pokeomons

--- src/routes/pokemons.js
+++ src/routes/pokemons.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+// Get all pokemons
+router.get("/", async (req, res, next) => {
+  try {
+    const apiResults = await pokemonsController.getAllApi();
+    return res.status(200).json({
+      statusCode: 200,
+      data: apiResults,
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
 module.exports = router;
