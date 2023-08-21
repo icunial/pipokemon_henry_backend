0bcdb9dcb318d74678b64f1ae9d7a3a242fc6e4d
Ignacio Cunial <igsecu@hotmail.com>
2023-08-21 20:28:48 -0300

    Updated getAllApi() function controller -> get pokemons types

--- src/controllers/pokemons.js
+++ src/controllers/pokemons.js
@@ -6,7 +6,7 @@ const getAllApi = async () => {
   try {
     const apiResults = await axios.get("https://pokeapi.co/api/v2/pokemon");
     if (apiResults) {
-      apiResults.data.results.forEach(async (r, index) => {
+      apiResults.data.results.forEach((r, index) => {
         results.push({
           id: index + 1,
           name: r.name,
@@ -15,25 +15,18 @@ const getAllApi = async () => {
           }.png`,
         });
       });
-      return results;
     }
+    for (result of results) {
+      const pokemonFound = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${result.id}`
+      );
+      result.types = pokemonFound.data.types.map((t) =>
+        t.type.name.toUpperCase()
+      );
+    }
+    return results;
   } catch (error) {
     throw new Error("Error trying to get all pokemons from API");
-  }
-};
-
-const getTypes = async (id) => {
-  try {
-    const pokemonFound = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${id}`
-    );
-    if (pokemonFound) {
-      return pokemonFound.data.types.map((t) => {
-        return t.type.name.toUpperCase();
-      });
-    }
-  } catch (error) {
-    throw new Error("Error trying to get pokemon types");
   }
 };
 
