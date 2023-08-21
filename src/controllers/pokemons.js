ee581b2d795e5bd3dfe05038604d519248dd661a
Ignacio Cunial <igsecu@hotmail.com>
2023-08-21 17:54:05 -0300

    Updated getAllApi() -> /src/controllers/pokemons.js

--- src/controllers/pokemons.js
+++ src/controllers/pokemons.js
@@ -2,11 +2,38 @@ const axios = require("axios");
 
 // Get All Dogs from API
 const getAllApi = async () => {
+  const results = [];
   try {
     const apiResults = await axios.get("https://pokeapi.co/api/v2/pokemon");
-    return apiResults;
+    if (apiResults) {
+      apiResults.data.results.forEach(async (r, index) => {
+        results.push({
+          id: index + 1,
+          name: r.name,
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+            index + 1
+          }.png`,
+        });
+      });
+      return results;
+    }
   } catch (error) {
     throw new Error("Error trying to get all pokemons from API");
+  }
+};
+
+const getTypes = async (id) => {
+  try {
+    const pokemonFound = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${id}`
+    );
+    if (pokemonFound) {
+      return pokemonFound.data.types.map((t) => {
+        return t.type.name.toUpperCase();
+      });
+    }
+  } catch (error) {
+    throw new Error("Error trying to get pokemon types");
   }
 };
 
