69f67548edb123f9f83ba5ec4fe201de53823806
Ignacio Cunial <igsecu@hotmail.com>
2023-08-22 23:00:13 -0300

    Created Pokemon Sequelize Model -> /src/models/Pokemon.js file

---
+++ src/models/Pokemon.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require("sequelize");
+const db = require("../db");
+
+const Pokemon = db.define(
+  "pokemon",
+  {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    life: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+    createdAt: true,
+    updatedAt: false,
+  }
+);
+
+module.exports = Pokemon;
