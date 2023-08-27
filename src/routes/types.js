450bc4907e8a99d6039a017e6bd59b6c99191857
Ignacio Cunial <igsecu@hotmail.com>
2023-08-26 21:56:12 -0300

    Created types.js file -> typesRouter

---
+++ src/routes/types.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const Pokemon = require("../models/Pokemon");
+const Type = require("../models/Type");
+
+const axios = require("axios");
+
+module.exports = router;
