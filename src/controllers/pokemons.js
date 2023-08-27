6c3f45b6043b2757c66f9270784cbc1491f6c129
Ignacio Cunial <igsecu@hotmail.com>
2023-08-26 21:46:37 -0300

    Updated orderPokemonsLessLife() function -> Added pokemons from db

--- src/controllers/pokemons.js
+++ src/controllers/pokemons.js
@@ -427,8 +427,9 @@ const orderPokemonsMoreLife = async () => {
 
 // Get Pokemons ordered by their life from less to more
 const orderPokemonsLessLife = async () => {
-  const results = [];
+  let results = [];
   try {
+    const dbResults = await utils.getFullAllDb();
     const apiResults = await axios.get(
       "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20"
     );
@@ -457,7 +458,7 @@ const orderPokemonsLessLife = async () => {
       result.defense = pokemonFound.data.stats[2].base_stat;
       result.speed = pokemonFound.data.stats[5].base_stat;
     }
-
+    results = [...dbResults, ...results];
     return results.sort((a, b) => {
       if (a.life > b.life) return 1;
       if (a.life < b.life) return -1;
