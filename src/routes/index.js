677399e188a6f4244e16b12a8eb4d1da851aa75e
Ignacio Cunial <igsecu@hotmail.com>
2023-08-26 21:49:39 -0300

    Updated index.js -> /src/routes -> Added typesRouter

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const pokemonsRouter = require("./pokemons");
+const typesRouter = require("./types");
 
 // Specify router root route
 router.use("/pokemons", pokemonsRouter);
+router.use("/types", typesRouter);
 
 module.exports = router;
